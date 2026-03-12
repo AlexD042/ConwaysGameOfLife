f436f1ce832b38b63e9c07972a97773813bbd37b
AlexD042 <drews.alex42@gmail.com>
2026-03-12 14:40:19 -0500

    Created blank screen and the simulation loop

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,26 @@
 
 int main() 
 {
-    // fixed?
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 800;
+    int FPS = 12;
+    Color GREY = {29, 29, 29, 255};
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Conway's Game of Life");
+    SetTargetFPS(FPS); // acts as an fps cap
+    
+    // Simulation Loop
+    while(WindowShouldClose() == false) {
+        // 1. Event Handling
+        
+        // 2. Updating State
+
+        // 3. Drawing
+
+        BeginDrawing();
+        ClearBackground(GREY);
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
